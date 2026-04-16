e0c548dcb44af1c767b8f1d10e9e72a4f957cc5f
Kings Oduah <developerkingsdevops@gmail.com>
2026-04-17 00:35:26 +0100

    added products route, implemented URL parsing, Query extraction, Conditional (dynamic) filtering based on request input

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 
 const sendJSON = (res, statusCode, data) => {
   res.statusCode = statusCode;
@@ -8,15 +9,46 @@ const sendJSON = (res, statusCode, data) => {
 
 //create a server
 const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
+
     if (req.method === "GET" && req.url === "/") {
         sendJSON(res, 200, { message: 'Home route!' });
     }
-    else if (req.method === "GET" && req.url === "/users") {
-    sendJSON(res, 200, [
+    else if (req.method === "GET" && pathname === "/users") {
+        console.log(query); // See query in terminal
+
+    let users = [
       { id: 1, name: "Kings" },
       { id: 2, name: "Alex" }
-    ]);
+    ];
+
+     // Simple filtering
+    if (query.name) {
+      users = users.filter(user => 
+        user.name.toLowerCase() === query.name.toLowerCase() );
+    }
+
+    sendJSON(res, 200, users);
   } 
+
+  else if (req.method === "GET" && pathname === "/products") {
+    console.log(query); // See query in terminal
+
+    let products = [
+      { id: 1, name: "Laptop" },
+      { id: 2, name: "Mouse" }
+    ];
+
+     // Simple filtering
+    if (query.name) {
+      products = products.filter(product => product.name === query.name);
+    }
+
+    sendJSON(res, 200, products);
+  } 
+
   else {
     sendJSON(res, 404, { error: "Route not found" });
   }
